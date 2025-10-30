f29cd347c6022513bb850f9006c8e437a187742d
Elena Kkk <elk230827@yandex.ru>
2025-10-30 13:33:03 +0300

    Исправления комментариев

--- test_bc.py
+++ test_bc.py
@@ -1,17 +1,11 @@
 from pytest import Collector
+from fixtures import BOOK, GENRE
 from main import BooksCollector
 import pytest
+
 BOOK = 'Гордость и предубеждение и зомби'
 GENRE = 'Фантастика'
 
-@pytest.fixture
-def collector_one_book():
-    collector = BooksCollector()
-
-    
-    collector.add_new_book(BOOK)
-    collector.set_book_genre(BOOK, GENRE)
-    return collector
 
 class TestBooksCollector:
 
@@ -21,17 +15,30 @@ class TestBooksCollector:
         collector.add_new_book('Гордость и предубеждение и зомби')
         collector.add_new_book('Что делать, если ваш кот хочет вас убить')
 
-        assert len(collector.get_books_genre()) == 2
+        assert len(collector.books_genre) == 2
 
-    def test_set_book_genre_set_one_genre(self, collector_one_book):
+    def test_get_book_genre_positive(self):
+        collector = BooksCollector()
+
+        collector.add_new_book(BOOK)
+        collector.set_book_genre(BOOK, GENRE)
+
+        assert collector.get_book_genre(BOOK) == GENRE  
+
+    def test_set_book_genre_one_genre(self):
+        collector = BooksCollector()
+        collector.add_new_book(BOOK)
+        collector.set_book_genre(BOOK, GENRE)
 
 
-        assert collector_one_book.get_book_genre(BOOK) == GENRE
+        assert collector.books_genre[BOOK] == GENRE
 
-    def test_get_books_with_specific_genre_correct_genre(self,collector_one_book):
+    def test_get_books_with_specific_genre_correct_genre(self):
+        collector = BooksCollector()
+        collector.add_new_book(BOOK)
+        collector.set_book_genre(BOOK, GENRE)
         
-        
-        res = collector_one_book.get_books_with_specific_genre(GENRE)
+        res = collector.get_books_with_specific_genre(GENRE)
 
         assert res[0] == BOOK
 
@@ -50,31 +57,45 @@ class TestBooksCollector:
 
         assert res[0] == books[2]
 
-    def test_add_book_in_favorites_positive(self, collector_one_book):
- 
-        res = collector_one_book.add_book_in_favorites(BOOK)
-        res = collector_one_book.get_list_of_favorites_books()
-        assert res[0] == BOOK
+    def test_add_book_in_favorites_positive(self):
+        collector = BooksCollector()
+        collector.add_new_book(BOOK)
+        collector.set_book_genre(BOOK, GENRE)
 
-    def test_get_list_of_favorites_books_positive(self, collector_one_book):
- 
-        collector_one_book.add_book_in_favorites(BOOK)
-        collector_one_book.delete_book_from_favorites(BOOK)
-        res = collector_one_book.get_list_of_favorites_books()
-        assert len(res) == 0
+        collector.add_book_in_favorites(BOOK)
+        
+        assert collector.favorites[0] == BOOK
 
-    def test_set_book_genre_wrong_genre(self, collector_one_book):
- 
-        collector_one_book.set_book_genre(BOOK, 'Мелодрама')
-        assert collector_one_book.get_book_genre(BOOK) == GENRE
+    def test_delete_book_from_favorites_positive(self):
+        collector = BooksCollector()
+        collector.add_new_book(BOOK)
+        collector.set_book_genre(BOOK, GENRE)
+       
+        collector.add_book_in_favorites(BOOK)
+        collector.delete_book_from_favorites(BOOK)
+        
+        assert len(collector.favorites) == 0
 
-    def test_add_book_in_favorites_wrong_book(self, collector_one_book):
+    def test_set_book_genre_wrong_genre(self):
+        collector = BooksCollector()
+        collector.add_new_book(BOOK)
+        collector.set_book_genre(BOOK, GENRE)
  
-        res = collector_one_book.add_book_in_favorites('Неправильная книга')
-        res = collector_one_book.get_list_of_favorites_books() 
-        assert len(res) == 0  
+        collector.set_book_genre(BOOK, 'Мелодрама')
+        assert collector.books_genre[BOOK] == GENRE
 
-    def test_get_book_genre_wrong_book(self, collector_one_book):
+    def test_add_book_in_favorites_wrong_book(self):
+        collector = BooksCollector()
+        collector.add_new_book(BOOK)
+        collector.set_book_genre(BOOK, GENRE)
  
-        res = collector_one_book.get_book_genre('Неправильная книга') 
+        collector.add_book_in_favorites('Неправильная книга')
+        assert len(collector.favorites) == 0
+
+    def test_get_book_genre_wrong_book(self):
+        collector = BooksCollector()
+        collector.add_new_book(BOOK)
+        collector.set_book_genre(BOOK, GENRE)
+ 
+        res = collector.get_book_genre('Неправильная книга') 
         assert res == None     
